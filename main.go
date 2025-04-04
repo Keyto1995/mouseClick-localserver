cfdef646a8bf1bcd4af21e87f40a37c705018070
Keyto <keyto1995@outlook.com>
2025-04-04 08:55:58 +0800

    feat(main): 添加 API 接口控制鼠标点击
    - 新增 /api/:action 路由，支持通过查询参数控制鼠标点击动作
    - 添加 robotgo 库以实现鼠标控制功能
    - 更新 go.mod 和 go.sum 文件，引入新的依赖

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-vgo/robotgo"
 )
 
 func main() {
@@ -15,5 +20,71 @@ func main() {
 			"msg":  "pong",
 		})
 	})
+
+	// /api/a?times=10
+	router.GET("/api/:action", func(c *gin.Context) {
+		action := c.Param("action")
+		queryTimes := c.Query("times")
+
+		times, err := strconv.Atoi(queryTimes)
+		if err != nil {
+			times = 1
+		}
+
+		var function func()
+
+		switch action {
+		case "a":
+			function = func() {
+				robotgo.Move(710, 782)
+			}
+		case "b":
+			function = func() {
+				robotgo.Move(710, 840)
+			}
+		case "c":
+			function = func() {
+				robotgo.Move(710, 894)
+			}
+		case "d":
+			function = func() {
+				robotgo.Move(710, 955)
+			}
+		}
+
+		for range times {
+			function()
+			robotgo.Click()
+			time.Sleep(time.Millisecond * 100)
+			robotgo.Move(135, 135)
+			robotgo.Click()
+			time.Sleep(time.Millisecond * 100)
+			robotgo.Move(1412, 135)
+			robotgo.Click()
+			time.Sleep(time.Millisecond * 100)
+			robotgo.Move(1095, 1006)
+			robotgo.Click()
+			time.Sleep(time.Millisecond * 100)
+
+			function()
+			robotgo.Click()
+			time.Sleep(time.Millisecond * 100)
+			robotgo.Move(1412, 135)
+			robotgo.Click()
+			time.Sleep(time.Millisecond * 100)
+			robotgo.Move(135, 135)
+			robotgo.Click()
+			time.Sleep(time.Millisecond * 100)
+			robotgo.Move(1095, 1006)
+			robotgo.Click()
+			time.Sleep(time.Millisecond * 100)
+		}
+
+		c.JSON(200, gin.H{
+			"code": 0,
+			"msg":  fmt.Sprintf("/api/%s?times=%d", action, times),
+		})
+	})
+
 	router.Run()
 }
